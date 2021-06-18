1795e48da44d5040ae5c9ec64c2cef240422a520
Clustered Networks <68355792+clusterednetworks@users.noreply.github.com>
2021-06-18 13:17:38 -0600

    Add files via upload

---
+++ october_cms_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# OctoberCMS Auto Update (Bash) Script
+# by ClusteredNetworks.com (info@clusterednetworks.com)
+#
+# Performs an automatic update of your OctoberCMS Websites on a server. 
+# Simply modify, add or remove the proper paths and lines to the script with the
+# path(s) to your website(s) on your server.
+# make the script executable (chmod +x) and run a cronjob
+# e.g. (0 23 * * 5 /etc/updateoctober.sh >/dev/null 2>&1)
+#----------------------------------------
+# OPTIONS
+#----------------------------------------
+PHP_PATH='/usr/bin/php'
+PHP_UPDATE='artisan october:update'
+#----------------------------------------
+$PHP_PATH -q /var/www/ronbillings.com/$PHP_UPDATE
+$PHP_PATH -q /var/www/clusterednetworks2/$PHP_UPDATE
+$PHP_PATH -q /var/www/repaintspecialists2/$PHP_UPDATE
+exit
